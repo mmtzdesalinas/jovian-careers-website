eb12e5a655adfd3f16ad7c87552e00afdcce554b
mmtzdesalinas <132124697+mmtzdesalinas@users.noreply.github.com>
2023-05-20 18:23:50 +0000

    changed things

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ JOBS=[
   {'id':1,'title':'Data Analyst','location':'Madrid','salary':'30.000 EUR'},
   {'id':2,'title':'Front End Engineer','location':'Madrid','salary':'35.000 EUR'},
   {'id':3,'title':'Back End Engineer','location':'Barcelona','salary':'40.000 EUR'},
-  {'id':4,'title':'Solutions Arquitect','location':'Bilbao'}
+  {'id':4,'title':'Solutions Arquitect','location':'Remote'}
 ]
 @app.route("/")
 def hello_world():
